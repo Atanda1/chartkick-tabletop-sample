ec1037dfe3f68d5ab5f7bf8d7cb4851ed8473878
David Atanda <atandadave@gmail.com>
2021-05-22 11:54:47 +0100

    Project initiated

---
+++ src/components/PieChart.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1>Pie chart here</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "PieChart",
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <PieCHart />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import PieCHart from "@/components/PieChart.vue";
+
+export default {
+  name: "Home",
+  components: {
+    PieCHart,
+  },
+};
+</script>
