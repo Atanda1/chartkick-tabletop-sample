054e41feb7615d8e808607274a21b5d0bb5fcc7b
David Atanda <atandadave@gmail.com>
2021-05-25 18:05:20 +0100

    Downloadable

--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="home">
     <h1>Pie chart here</h1>
-    <pie-chart :data=returnedData></pie-chart>
+    <pie-chart :data=returnedData :download="true"></pie-chart>
   </div>
 </template>
 
+<style>
+div {
+  margin: 0 auto;
+  width: 70%;
+}
+</style>
 <script>
 export default {
   name: "PieChartComponent",
